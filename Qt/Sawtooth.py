2c94d6c7b220262f267689e0671653eb4a347d91
Jason Knoll <jasonknoll98@aol.com>
2024-01-02 13:41:39 -0500

    Create Sawtooth.py

---
+++ Qt/Sawtooth.py
@@ -0,0 +1,13 @@
+# New chess bot with GUI
+
+# Minimum viable product can:
+# 0. Possess a working calculation function lmao
+# 1. Handle challenges from LiChess
+# 2. Display the board of the current match being played
+# 3. Display data about search and eval
+
+# Macro steps
+# 1. Build a Qt gui window
+# 2. Get the calculation/best-move functions working (instead of just repeating moves)
+# 3. Display current board state (including opponent -- would be cool)
+# 4. Display calculation data
